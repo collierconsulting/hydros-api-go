2e9c5a1a2662357ebcae99756d7f936d96b7c705
Jonathan Bailey <jonathan@collierconsulting.com>
2018-11-19 15:42:43 -0600

    model.Spec should not be serialized in JSON

--- model.go
+++ model.go
@@ -4,8 +4,8 @@ import "time"
 
 // DefaultModelBase Standard model base struct
 type DefaultModelBase struct {
-	Spec      *ServiceSpec
-	ID        uint      `json:"id"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	Spec      *ServiceSpec `json:"-"`
+	ID        uint         `json:"id"`
+	CreatedAt time.Time    `json:"createdAt"`
+	UpdatedAt time.Time    `json:"updatedAt"`
 }
